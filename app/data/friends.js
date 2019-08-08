1e4a017c1c07ef2b3db61186203692dc54a3598a
Kevin Steele <kevdsteele@gmail.com>
2019-08-08 14:39:05 -0400

    Worked through logic

---
+++ app/data/friends.js
@@ -0,0 +1,35 @@
+var friendsData = [
+{
+"name" : "Jane Smith",
+"photo" : "https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+"scores" : [5,4,3,2,1,2,3,4,5,3]
+},
+{
+"name" : "John Smith",
+"photo" : "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+"scores" : [4,5,3,3,4,5,2,1,2,3]
+},
+{
+"name" : "June Jones",
+"photo" : "https://images.pexels.com/photos/1239291/pexels-photo-1239291.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+"scores" : [3,5,4,2,1,2,3,2,4,5]
+},
+{
+"name" : "Bob Jones",
+"photo" : "https://images.pexels.com/photos/1222271/pexels-photo-1222271.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+"scores" : [2,1,2,1,3,4,3,5,4,3]
+},
+{
+"name" : "Jim Jimmers",
+"photo" : "https://images.pexels.com/photos/1043474/pexels-photo-1043474.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+"scores" : [4,2,3,1,3,1,3,2,1,3]
+},
+{
+"name" : "Tammy Tammers",
+"photo" : "https://images.pexels.com/photos/1036623/pexels-photo-1036623.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+"scores" : [4,5,2,5,1,4,2,5,2,3]
+},
+
+]
+
+module.exports =friendsData
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,68 @@
+// ===============================================================================
+// LOAD DATA
+// We are linking our routes to a series of "data" sources.
+// These data sources hold arrays of information on table-data, waitinglist, etc.
+// ===============================================================================
+
+var friendsData = require("../data/friends");
+
+
+
+// ===============================================================================
+// ROUTING
+// ===============================================================================
+
+module.exports = function(app) {
+  // API GET Requests
+  // Below code handles when users "visit" a page.
+  // In each of the below cases when a user visits a link
+  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
+  // ---------------------------------------------------------------------------
+
+  app.get("/data/friends", function(req, res) {
+    res.json(friendsData);
+  });
+
+ 
+
+  // API POST Requests
+  // Below code handles when a user submits a form and thus submits data to the server.
+  // In each of the below cases, when a user submits form data (a JSON object)
+  // ...the JSON is pushed to the appropriate JavaScript array
+  // (ex. User fills out a reservation request... this data is then sent to the server...
+  // Then the server saves the data to the tableData array)
+  // ---------------------------------------------------------------------------
+
+  app.post("/data/friends", function(req, res) {
+  var closestScore =100
+  var closestName =""
+  var closestPhoto=""
+  
+  friendsData.forEach(function (friend){
+    var tempScore =0
+    console.log(friend.name)
+    console.log(friend.scores)
+    for (i =0; i < friend.scores.length; i++) {
+        tempScore+=Math.abs(friend.scores[i] - req.body.scores[i])
+    }
+     console.log("Temp score for " + friend.name+ " is " + tempScore)
+     if (tempScore < closestScore) {
+         closestScore=tempScore
+         closestName=friend.name
+         closestPhoto=friend.photo
+     }
+        
+  })   
+
+
+
+  console.log(req.body)  
+  console.log("The closest match is " + closestName + " photo " + closestPhoto)
+  var data = {"name" : closestName, "photo" : closestPhoto}
+  res.json(data)
+  
+  friendsData.push(req.body)
+ 
+  });
+
+}
